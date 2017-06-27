6647192bed9b6275dbfd41d32d9dcd74a45da5d5
Carlos Arcila Calderón <carcila@gmail.com>
2017-06-27 12:59:59 +0200

    Add files via upload

---
+++ scripts/install.py
@@ -0,0 +1,16 @@
+'''
+INSTALAR ESTAS LIBRERIAS >PYCHARM >Preferences >Project: Python > Project: Interpreter
+Nltk
+Tweepy
+Numpy
+Statistics
+Sklearn
+'''
+
+#Para terminar de instalar los módulos de NLTK ejecutar este script o en consola de Pyhton los siguientes comandos:
+import nltk
+nltk.download()
+
+#d (for download)
+#all (for download everything) #l = all #Identifier> all
+
