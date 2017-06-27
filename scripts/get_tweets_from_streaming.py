241af7391d1f89c2c56814528959ff723c024a91
Carlos Arcila Calderón <carcila@gmail.com>
2017-06-27 11:15:13 +0200

    Update get_tweets_from_streaming.py

--- scripts/get_tweets_from_streaming.py
+++ scripts/get_tweets_from_streaming.py
@@ -26,7 +26,7 @@ class listener(StreamListener):
         return(True)
 
     def on_error(self, status):
-        print status
+        print(status)
 
 auth = OAuthHandler(ckey, csecret)
 auth.set_access_token(atoken, asecret)
